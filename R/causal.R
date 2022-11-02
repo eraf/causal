e9add3081a1473e928ff3eee7a86602b91eb7d9c
Eraf Hossain <eraf.hossain@gmail.com>
2022-11-02 12:28:58 +0600

    done with or

--- R/causal.R
+++ R/causal.R
@@ -1,35 +1,4 @@
 #' @importFrom dplyr .data
-calc_or <- function(treatment, y, n, treatment_ref_lvl = NULL,
-                    y_ref_lvl = NULL) {
-  check_param_null(treatment_ref_lvl, "treatment reference level")
-  check_param_null(y_ref_lvl, "y reference level")
-  dplyr::tibble(treatment, y, n) %>%
-    dplyr::mutate(
-      treatment = dplyr::if_else(treatment == treatment_ref_lvl, 0, 1),
-      y = dplyr::if_else(y == y_ref_lvl, 0, 1)
-    ) %>%
-    dplyr::summarise(
-      or = (.data$n[4] * .data$n[1]) / (.data$n[2] * .data$n[3])
-    ) %>%
-    dplyr::pull(.data$or)
-}
-
-#' @importFrom dplyr .data
-dcalc_or <- function(data, treatment, y, group = NULL) {
-  check_data(data)
-  data %>%
-    dplyr::select({{ treatment }}, {{ y }}, {{ group }}) %>%
-    dplyr::group_by({{ group }}) %>%
-    dplyr::count({{ treatment }}, {{ y }}) %>%
-    dplyr::rename("x" = {{ treatment }}, "y" = {{ y }}) %>%
-    dplyr::summarise(
-      or = calc_or(.data$x, .data$y, .data$n)
-    )
-}
-
-
-
-#' @importFrom dplyr .data
 gen_pseudo_popn <- function(data, treatment, y, group = NULL) {
   data %>%
     dplyr::count({{ group }}, {{ treatment }}, {{ y }}) %>%
---
+++ tests/testthat/test-calc_or.R
@@ -0,0 +1,137 @@
+library(dplyr)
+
+wcgs <- wcgs %>% filter(!is.na(arcus))
+
+test_that("Checking for simple single rr calculation", {
+  # manual
+  dt11 <- wcgs %>%
+    count(smoke, chd69) %>%
+    summarise(
+      or = (n[4] * n[1]) / (n[2] * n[3])
+    )
+
+  dt12 <- wcgs %>%
+    count(smoke, chd69) %>%
+    summarise (
+      or = calc_or(smoke, chd69, n, "No", "No")
+    )
+
+  dt13 <- wcgs %>%
+    dcalc_or(smoke, chd69 , "No", "No")
+  #doesn't work: unused arguments "No", "No"
+
+  expect_identical(dt11, dt12)
+  expect_identical(dt11, dt13)
+  expect_identical(dt12, dt13)
+})
+
+
+test_that("Checking for grouped rr calculation", {
+  # manual
+  dt21 <- wcgs %>%
+    group_by(arcus) %>%
+    count(chd69, smoke) %>%
+    summarise(
+      or = calc_or(smoke, chd69, n, "No", "No")
+    )
+
+  dt22 <- wcgs %>%
+    dcalc_or(smoke, chd69, "No", "No", group = arcus) #same issue
+
+  expect_identical(dt21, dt22)
+})
+
+
+test_that("Checking for calculation if ref level changed (single value)", {
+  # manual
+  dt31 <- wcgs %>%
+    mutate(
+      smoke = if_else(smoke == "No", "Yes", "No"),
+      chd69 = if_else(chd69 == "No", "Yes", "No")
+    ) %>%
+    count(smoke, chd69)%>% #corrected
+    summarise(
+      or = (n[4] * n[1]) / (n[2] * n[3])
+    )
+
+  dt32 <- wcgs %>%
+    count(smoke, chd69) %>%
+    summarise (
+      or = calc_or(smoke, chd69, n, "Yes", "Yes")
+    )
+
+  dt33 <- wcgs %>%
+    dcalc_or(smoke, chd69, "Yes", "Yes")
+
+  expect_identical(dt31, dt32)
+  expect_identical(dt31, dt33)
+  expect_identical(dt32, dt33)
+})
+
+
+test_that("Checking for calculation if ref level changed (grouped value)", {
+  # manual
+  dt41 <- wcgs %>%
+    group_by(arcus) %>%
+    count(chd69, smoke) %>%
+    summarise(
+      or = calc_or(smoke, chd69, n, "Yes", "Yes")
+    )
+
+  dt42 <- wcgs %>%
+    dcalc_or(smoke, chd69, "Yes", "Yes", group = arcus)
+
+  expect_identical(dt41, dt42, )
+})
+
+
+test_that("Expecting errors in calc_rr", {
+  expect_error(
+    wcgs %>%
+      count(smoke, chd69) %>%
+      summarise (
+        or = calc_or(smoke, chd69, n)
+      ),
+    "Could not determine the treatment reference level"
+  )
+
+  expect_error(
+    wcgs %>%
+      count(smoke, chd69) %>%
+      summarise (
+        or = calc_or(smoke, chd69, n, y_ref_lvl = "No")
+      ),
+    "Could not determine the treatment reference level"
+  )
+
+  expect_error(
+    wcgs %>%
+      count(smoke, chd69) %>%
+      summarise (
+        or = calc_or(smoke, chd69, n, "No")
+      ),
+    "Could not determine the y reference level"
+  )
+})
+
+
+test_that("Expecting errors in dcalc_rr", {
+  expect_error(
+    wcgs %>%
+      dcalc_or(smoke, chd69),
+    "Could not determine the treatment reference level"
+  )
+
+  expect_error(
+    wcgs %>%
+      dcalc_or(smoke, chd69, y_ref_lvl = "No"),
+    "Could not determine the treatment reference level"
+  )
+
+  expect_error(
+    wcgs %>%
+      dcalc_or(smoke, chd69, "No"),
+    "Could not determine the y reference level"
+  )
+})
+
